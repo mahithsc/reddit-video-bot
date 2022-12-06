8453eb0b6eac2aca43c25c1cca4fd4d123e7bb62
mahithsc <mahith.chitrapu@gmail.com>
2022-12-06 01:42:54 -0800

    writing a script which creates a video as well

--- make_movie.py
+++ make_movie.py
@@ -1,1 +1,15 @@
-from moviepy.editor import VideoClip
+from moviepy.editor import VideoClip, concatenate_videoclips, VideoFileClip, afx, TextClip, CompositeVideoClip, AudioFileClip, ImageClip
+
+titleVideoClip = VideoFileClip("minecraft.mp4").subclip(10,15).without_audio()
+titleImageClip = ImageClip("./video_assets/title.png", duration=5).set_position('center')
+titleComposite = CompositeVideoClip([titleVideoClip, titleImageClip])
+
+commentVideoClip = VideoFileClip("minecraft.mp4").subclip(15,20).without_audio()
+commentImageClip = ImageClip("./video_assets/comment.png", duration=5).set_position('center')
+commentComposite = CompositeVideoClip([commentVideoClip, commentImageClip])
+
+final = concatenate_videoclips([titleComposite, commentComposite])
+
+
+
+final.write_videofile("final.mp4")
